085cf09d0e3cf4b4a263867c478b017b7231dda0
Sabirat <rubya001@umn.edu>
2017-10-26 12:31:14 -0500

    first commit

---
+++ client/src/Root.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+var amazingInlineJsStyle = {
+  border: '1px solid purple',
+  padding: '10px',
+  backgroundColor: '#ffffcc'
+}
+
+class Root extends Component {
+  constructor() {
+    super();
+    this.state = { message: '' }
+  }
+
+  componentDidMount() {
+    var self = this;
+    axios.get('/api/example')
+    .then(function (response) {
+      console.log(response);
+      self.setState({message: response.data.message});
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+  render() {
+    return (
+      <div className="Root">
+        <p>
+          Message from server:
+          <span style={amazingInlineJsStyle}>{this.state.message}</span>
+        </p>
+        <p>
+          Root! <Link to={`/foo`}>a link to foo</Link>
+        </p>
+      </div>
+    );
+  }
+}
+
+
+/*Root.propTypes = {
+  message: React.PropTypes.string
+};*/
+export default Root;
+//export default RootContainer;
---
+++ client/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import { Route,BrowserRouter, Link, Switch } from 'react-router-dom';
+
+import Root from './Root'
+import Foo from './Foo'
+
+
+ReactDOM.render(
+
+      <div>
+        <App />
+        <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Root}/>
+          <Route path='/foo' component={Foo}/>
+        </Switch>
+        </BrowserRouter>
+    </div>
+
+      /*<Route path="/" component={Root}/>
+      <Route path="/foo" component={Foo}/>*/
+, document.getElementById('root'));
+registerServiceWorker();
