246aa6f08566713f41ed66ec7eef8fa46799d3ae
Sabirat <rubya001@umn.edu>
2017-10-26 22:44:29 -0500

    changes to www

--- server/app.js
+++ server/app.js
@@ -7,7 +7,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 var api = require('./routes/api');
 
 var app = express();
@@ -24,8 +23,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+//app.use('/', index);
+//app.use('/users', users);
 app.use('/api', api);
 
 // catch 404 and forward to error handler
